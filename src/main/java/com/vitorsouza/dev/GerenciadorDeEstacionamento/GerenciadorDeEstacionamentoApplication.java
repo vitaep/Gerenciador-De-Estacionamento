4322baa2f0f34e5ad4ca14a7c1bf155e444af24a
Vitor Bomfim <souzavitorb1@gmail.com>
2025-03-02 00:39:19 -0300

    first commit

---
+++ src/main/java/com/vitorsouza/dev/GerenciadorDeEstacionamento/GerenciadorDeEstacionamentoApplication.java
@@ -0,0 +1,13 @@
+package com.vitorsouza.dev.GerenciadorDeEstacionamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeEstacionamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeEstacionamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitorsouza/dev/GerenciadorDeEstacionamento/GerenciadorDeEstacionamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorsouza.dev.GerenciadorDeEstacionamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeEstacionamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
